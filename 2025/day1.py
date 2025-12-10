ebb08409bf4685ef02bc6be877e4c1ef37a71ba7
Julian Reed <juliantreed@gmail.com>
2025-12-09 21:01:25 -0800

    Final solution for day 1

--- 2025/day1.py
+++ 2025/day1.py
@@ -2,7 +2,7 @@
 Day 1 Solution.
 Link: https://adventofcode.com/2025/day/1
 """
-from pathlib import Path
+DIAL_SIZE = 100
 
 def read_input() -> str:
     with open("day1Input.txt", 'r', encoding='utf-8') as f:
@@ -12,6 +12,21 @@ def read_input() -> str:
 # returns the solution for today's puzzle, which is how many times the dial
 # is left pointing at 0 after any rotation
 def solution() -> int:
+    zero_count = 0
+    pos = 50
+    input = read_input()
+    turns = input.split("\n")
+    for turn in turns:
+        if turn == '':
+            continue
+        dir = turn[0]
+        dist = int(turn[1:]) % DIAL_SIZE
+        if dir == 'R':
+            pos = (pos + dist) % 100
+        else:
+            pos = 100 - dist + pos if dist > pos else pos - dist
+        zero_count = zero_count + 1 if pos == 0 else zero_count
+    return zero_count
 
 if __name__ == "__main__":
     output = solution()
