cbd0af930ee9224244ce48c1003056e959123f04
Julian Reed <juliantreed@gmail.com>
2025-12-10 08:42:46 -0800

    Day 2 part 1 solution

--- 2025/day-2/day2Part1.py
+++ 2025/day-2/day2Part1.py
@@ -10,10 +10,27 @@ def read_input(path) -> str:
 # within that range
 def get_invalid_ids(start: int, end: int) -> int:
     """
-    Approach: get the prefix for this range
+    Approach: get the prefix for this range (the first half of the start string)
+    and generate all invalid IDs until we go above the end of the range
     """
     tot = 0
-
+    as_str = str(start)
+    as_str = as_str[:len(as_str) // 2]
+    if as_str == '':
+        as_str = '1'
+    cur = int(as_str)
+    while True:
+        to_test = int(str(cur) + str(cur))
+        # skip cases where splitting the string in half on odd-length start 
+        # results in considering a range before start
+        if to_test < start:
+            cur += 1
+            continue
+        if to_test <= end:
+            tot += to_test
+            cur += 1
+        else:
+            break
     return tot
 
 def solution(input_path):
@@ -21,7 +38,7 @@ def solution(input_path):
     input = read_input(input_path)
     for bound in input.split(','):
         start, end = bound.split('-')
-        bounds.append((start, end))
+        bounds.append((int(start), int(end)))
     tot = 0
     for s, e in bounds:
         tot += get_invalid_ids(s,e)
