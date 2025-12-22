5199da617ddf2e2b050d40e56294df37135bc98f
Julian Reed <juliantreed@gmail.com>
2025-12-21 19:58:24 -0500

    Incomplete day 10, part 1 solution using dp

--- 2025/day-10/part1.py
+++ 2025/day-10/part1.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+UPPER_LIMIT = 100
 def read_input(path: str) -> list[tuple[int, int]]:
     machines = []
     with open(path, 'r', encoding='utf-8') as f:
@@ -20,16 +22,56 @@ def read_input(path: str) -> list[tuple[int, int]]:
             machines.append(machine_dir)
     return machines
 
-def find_fewest_presses(machine) -> int:
-    # approach:
-    return 0
+# helper function to perform binary xor on two string inputs
+def xor(s1, s2) -> str:
+    n1 = int(s1, 2)
+    n2 = int(s2, 2)
+    res = n1 ^ n2
+    # convert to binary, cut off "0b"
+    bin_res = bin(res)[2:]
+    # pad with correct number of bits
+    return bin_res.zfill(len(s1))
+
+def find_fewest_presses(cache, buttons, state, presses, seen) -> int:
+    if presses >= UPPER_LIMIT:
+        return presses
+    if state in cache:
+        print(f"State already in cache: {state}, value = {cache[state]}")
+        return cache[state]
+    min_val = UPPER_LIMIT
+    for button in buttons:
+        new_state = xor(state, button)
+        # prune this branch if we have already seen this state, as this creates a cycle and can't be optimal
+        print(f"xor of {state}, {button} = {new_state}. Seen = {seen}")
+        if new_state in seen:
+            print("Cycle detected, continuing")
+            continue
+        if new_state not in cache or cache[new_state] == UPPER_LIMIT:
+            # print(f"State {new_state} not found in cache. Calculating...")
+            # add new state to the cache
+            cache[new_state] = find_fewest_presses(cache, buttons, deepcopy(new_state), presses + 1, deepcopy(seen | {new_state}))
+        min_val = min(min_val, cache[new_state] + 1)
+    cache[state] = min_val
+    print(f"Adding state {state} to cache, value = {min_val}")
+    return cache[state]
 
 def solution(machines) -> int:
-    return sum(find_fewest_presses(machine) for machine in machines)
+    # approach: try all possible positions, return the minimum from the current spot. BFS.
+    # uses an UPPER LIMIT I defined since recursion could go on forever
+    tot = 0
+    for i, machine in enumerate(machines):
+        init_state = "0" * len(machine["indicator"])
+        print("init_state:", init_state)
+        cache = {init_state:0}
+        local_tot = find_fewest_presses(cache, machine["buttons"], machine["indicator"], 0, set())
+        tot += local_tot
+        print(f"total for machine {i}: {local_tot}")
+        print("cache:", cache)
+    return tot
 
 if __name__ == "__main__":
-    # input_path = "input.txt"
-    input_path = "small_input.txt"
+    input_path = "input.txt"
+    # input_path = "small_input.txt"
     machines = read_input(input_path)
     output = solution(machines)
     print("output = ", output)
